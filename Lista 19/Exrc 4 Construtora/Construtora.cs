60bb897d00783416380403cd68b95c3a7b4c1176
radmila, a da pressão baixa <milaejared@outlook.com>
2019-10-11 15:31:50 -0300

    Exercício 4

---
+++ Lista 19/Exrc 4 Construtora/Construtora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_4_Construtora
+{
+    class Construtora
+    {
+        private List<Funcionario> funcs;
+        public void Inserir(Funcionario f) { }
+        public List<Funcionario> Funcionarios() { }
+        public List<Engenheiro> Engenheiros() { }
+        public List<Motorista> Motoristas() { }
+    }
+}
---
+++ Lista 19/Exrc 4 Construtora/Engenheiro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_4_Construtora
+{
+    class Engenheiro
+    {
+        private string crea;
+        public void SetCrea(string c) { }
+        public string GetCrea() { }
+        public override string ToString()
+        {
+            return crea;
+        }
+    }
+}
