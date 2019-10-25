1c8f82a497b0b5b75c6dcd5e0b65efa334e68354
socorro jesus <milaejared@outlook.com>
2019-10-25 15:34:58 -0300

    Ver ArrayList

---
+++ Lista 20/Exrc 2 Pilha utilizando uma Lista/Pilha1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_2_Pilha_utilizando_uma_Lista
+{
+    class Pilha1<T>
+    {
+        private List<T> lista;
+        ArrayList v = new ArrayList();
+        private int k;
+        public int Count()
+        {
+            ArrayList r = new ArrayList(k);
+            Array.Copy(v, r, k);
+            return int.Parse(r);
+        }
+        public T Top() { return v[v.Length-1]; }
+        public void Pop() { v.Remove(); }
+        public void Push(T obj) { if (k < 50) v[k++] = obj; }
+    }
+}
