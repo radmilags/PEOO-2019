15a919ad80940054fd9d750c5d6ca8361b213e32
socorro jesus <milaejared@outlook.com>
2019-10-25 14:23:20 -0300

    O visual studio está tendo problemas, salvar a cada 5min

---
+++ Lista 20/Exrc 1 Conversor Binário/Conversor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_1_Conversor_Binário
+{
+    class Conversor
+    {
+        private int num;
+        private Stack <int> pilha;
+    }
+}
