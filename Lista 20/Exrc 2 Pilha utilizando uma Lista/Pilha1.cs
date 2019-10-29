3af56c617b17852e230c780a2b51339db69a7a0c
socorro jesus <milaejared@outlook.com>
2019-10-29 15:31:03 -0300

    Concluído

--- Lista 20/Exrc 2 Pilha utilizando uma Lista/Pilha1.cs
+++ Lista 20/Exrc 2 Pilha utilizando uma Lista/Pilha1.cs
@@ -10,8 +10,19 @@ namespace Exrc_2_Pilha_utilizando_uma_Lista
     {
         private List<T> lista = new List<T>();
         public int Count() { return lista.Count; }
-        public T Top() { return lista.Peek(); }
-        public void Pop() { lista.Remove(); }
-        public void Push(T obj) { lista.Push(obj); }
+        public T Top()
+        {
+            T t;
+            t = lista[Count() - 1];
+            return t;
+        }
+        public T Pop()
+        {
+            T t;
+            t = lista[Count() - 1];
+            lista.RemoveAt(Count() - 1);
+            return t;
+        }
+        public void Push(T obj) { lista.Insert(Count(), obj); }
     }
 }
