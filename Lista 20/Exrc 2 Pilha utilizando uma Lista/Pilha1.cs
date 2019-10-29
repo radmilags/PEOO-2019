d3c72b0203672fddf6b25ff4320155c269ce5bbc
socorro jesus <milaejared@outlook.com>
2019-10-29 15:16:52 -0300

    MDS faltei o cap de dani

--- Lista 20/Exrc 2 Pilha utilizando uma Lista/Pilha1.cs
+++ Lista 20/Exrc 2 Pilha utilizando uma Lista/Pilha1.cs
@@ -8,11 +8,8 @@ namespace Exrc_2_Pilha_utilizando_uma_Lista
 {
     class Pilha1<T>
     {
-        private List<T> lista;
-        public int Count()
-        {
-            foreach (int c in lista) return c;
-        }
+        private List<T> lista = new List<T>();
+        public int Count() { return lista.Count; }
         public T Top() { return lista.Peek(); }
         public void Pop() { lista.Remove(); }
         public void Push(T obj) { lista.Push(obj); }
