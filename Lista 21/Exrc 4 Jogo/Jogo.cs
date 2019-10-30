8d2021c506db828e97bfb259aee4814f13e41260
socorro jesus <milaejared@outlook.com>
2019-10-30 08:16:21 -0300

    É difícil pensar com fome

---
+++ Lista 21/Exrc 4 Jogo/Jogo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_4_Jogo
+{
+    class Jogo
+    {
+        private string nome;
+        private List<Jogador> jogs = new List<Jogador>();
+        private int k;
+        public Jogo(string n) { nome = n; }
+        public void Inserir(Jogador j)
+        {
+            jogs[k++] = j;
+        }
+        public List<Jogador> Listar()
+        {
+            List<Jogador> r = new List<Jogador>(k);
+            Array.Copy(jogs, r, k);
+            return r;
+        }
+    }
+}
