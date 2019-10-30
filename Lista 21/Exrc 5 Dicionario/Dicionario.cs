5f208ff33a5634d1aca1020f0fccb023feb2491f
socorro jesus <milaejared@outlook.com>
2019-10-30 10:14:58 -0300

    Preciso concluir

---
+++ Lista 21/Exrc 5 Dicionario/Dicionario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_5_Dicionario
+{
+    class Dicionario<k,v>
+    {
+        private List<k> chaves = new List<k>();
+        private List<v> valores = new List<v>();
+    }
+}
