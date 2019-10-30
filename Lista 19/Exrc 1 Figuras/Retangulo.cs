d6c3e7fbb6862317934b64ca3e2ceb4418e68080
socorro jesus <milaejared@outlook.com>
2019-10-30 09:07:23 -0300

    Dei um espaço

--- Lista 19/Exrc 1 Figuras/Retangulo.cs
+++ Lista 19/Exrc 1 Figuras/Retangulo.cs
@@ -12,7 +12,7 @@ namespace Exrc_1_Figuras
         public Retangulo(double b, double h) { if (b > 0) this.b = b; if (h > 0) this.h = h; }
         public double GetBase() { return b; }
         public double GetAltura() { return h; }
-        public double CalcArea() { return b *h; }
+        public double CalcArea() { return b * h; }
         public double CalcDiagonal() { return Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2)); }
         public override string ToString()
         {
