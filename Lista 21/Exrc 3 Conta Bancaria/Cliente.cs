fcf652d2473fd0d94484de9d277ddd5700bbfb65
socorro jesus <milaejared@outlook.com>
2019-10-30 07:41:12 -0300

    Exercício 3

---
+++ Lista 21/Exrc 3 Conta Bancaria/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_3_Conta_Bancaria
+{
+    class Cliente
+    {
+        private string nome, email, fone;
+        private int k;
+        ContaBancaria[] c = new ContaBancaria[50];
+        public Cliente(string n, string e, string f)
+        {
+            nome = n; email = e; fone = f;
+        }
+        public void Inserir(ContaBancaria conta)
+        {
+            if (k < 50) c[k++] = conta;
+        }
+    }
+}
