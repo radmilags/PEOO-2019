a22f981124a7696362a340363fa621ff50fdb131
Radmila <milaejared@outlook.com>
2019-10-31 17:54:34 -0300

    mudar get e set para um construtor

--- Lista 22/Exrc 1 Jogos/NGame.cs
+++ Lista 22/Exrc 1 Jogos/NGame.cs
@@ -9,10 +9,11 @@ namespace Exrc_1_Jogos
     class NGame
     {
         private List<Game> v = new List<Game>();
+        private Game g = new Game();
         private PGame p;
-        public List<Game> Select() //Lista todos os jogos em ordem alfabética;
+        public List<Game> Select() //Lista todos os jogos em ordem alfabética; usar List.OrderBy
         {
-
+            return v.OrderBy(g.nome);
         }
         public List<Game> SelectData() //Lista todos os jogos por ordem de compra;
         {
