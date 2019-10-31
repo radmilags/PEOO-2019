361d9ca9f39fa8a3b36293c46544c02d7f4b6830
Radmila Gama <milaejared@outlook.com>
2019-10-31 18:46:23 -0300

    Seguimento

---
+++ Lista 22/Exrc 2 Livraria/NLivro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_2_Livraria
+{
+    class NLivro
+    {
+        private List<Livro> v;
+        private PLivro p;
+        public List<Livro> Select() { }
+    }
+}
