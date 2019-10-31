12efb2c73a8f59a74cbf1fa81b0cc4b366786f1d
socorro jesus <milaejared@outlook.com>
2019-10-31 11:27:48 -0300

    Irei concluir mais tarde

---
+++ Lista 22/Exrc 1 Jogos/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exrc_1_Jogos
+{
+    class Game
+    {
+        private int id, estrelas;
+        private string nome, fabricante;
+        private DateTime DataCompra;
+    }
+}
