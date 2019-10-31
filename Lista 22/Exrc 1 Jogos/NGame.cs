d1c55515e11c8da970752ac14aea20d191a0f0cf
Radmila Gama <milaejared@outlook.com>
2019-10-31 18:58:27 -0300

    Não sei mais o que fazer

--- Lista 22/Exrc 1 Jogos/NGame.cs
+++ Lista 22/Exrc 1 Jogos/NGame.cs
@@ -13,18 +13,18 @@ namespace Exrc_1_Jogos
         private PGame p;
         public List<Game> Select() //Lista todos os jogos em ordem alfabética; usar List.OrderBy
         {
-            return v.OrderBy();
+            return v.OrderBy(); // Não sei como continuar
         }
         public List<Game> SelectData() //Lista todos os jogos por ordem de compra;
         {
-
+            return v.OrderBy();
         }
         public List<Game> Top10() //Lista os 10 melhores jogos
         {
 
         }
         public void Insert(Game g) { v.Add(g); }
-        public void Update(Game g) { }
+        public void Update(Game g) { v.Insert(v.IndexOf(g), g); }
         public void Delete(Game g) { v.Remove(g); }
     }
 }
